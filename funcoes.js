ad593e3f1a98d120afd6057f63c596ba0c3e6b0b
Juliana Nascimento <juliananr6@gmail.com>
2025-02-28 11:41:09 -0300

    função do botão

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+function redirecionarInstagram() {
+    window.location.href = "https://www.instagram.com";
+}
+
+function redirecionarGithub() {
+    window.location.href = "https://github.com/julianaroberto";
+}
