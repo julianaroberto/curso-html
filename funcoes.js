23651ee524c885823b5043cf47c928213d3cc9d5
Juliana Nascimento <juliananr6@gmail.com>
2025-03-06 19:11:28 -0300

    alteração de botões

--- funcoes.js
+++ funcoes.js
@@ -1,7 +1,12 @@
-function redirecionarInstagram() {
-    window.location.href = "https://www.instagram.com";
-}
-
 function redirecionarGithub() {
     window.location.href = "https://github.com/julianaroberto";
+}
+
+
+function redirecionarLinkedin() {
+    window.location.href = "https://www.linkedin.com/in/juliana-roberto-administrativo/";
+}
+
+function redirecionarTwitch() {
+    window.location.href = "https://www.twitch.tv/";
 }
